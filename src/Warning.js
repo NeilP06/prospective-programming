afcd5f8504f31732fd32b4873d2653c696c35b4f
NlytxGames <58278838+NlytxGames@users.noreply.github.com>
2023-07-08 18:23:47 -0500

    Add routing

--- src/Warning.js
+++ src/Warning.js
@@ -11,7 +11,7 @@ export default function Warning() {
     }
     return (
         <div class="flex flex-col items-center justify-center mt-32">
-            <div class="flex items-center w-96 px-6 py-14 rounded bg-slate-200 dark:bg-gray-800 border-2 border-white dark:border-gray-600">
+            <div class="flex items-center w-4/6 px-6 py-14 rounded bg-slate-200 dark:bg-gray-800 border-2 border-white dark:border-gray-600">
                 <p class="text-center text-lg font-medium text-black dark:text-white">ProspectiveProgramming does not function on mobile phone devices at this current moment. To use our application, please use a mobile tablet device or a computer to proceed. <i>We are sorry for this inconvenience and we will address this soon.</i></p>
             </div>
             <div class="flex flex-row items-center justify-center mt-5">
