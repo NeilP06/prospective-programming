d85cda6d3d849148370a2bf22d0d0686fe4bd60d
NlytxGames <58278838+NlytxGames@users.noreply.github.com>
2023-07-27 20:08:01 -0500

    Minor changes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { ClerkProvider, SignedIn, SignedOut, useUser } from "@clerk/clerk-react";
 import Footer from "./Footer.js";
 import Greetings from "./Customization.js";
 import Lesson from "./Lesson.js";
@@ -14,41 +14,12 @@ if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
 const key = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 export default function App() {
-  const desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-  var username = "Neil";
   document.body.classList.add("bg-slate-300", "dark:bg-gray-900");
   if (window.innerWidth > 800) {
     return (
       <ClerkProvider publishableKey={key}>
         <Analytics/>
-        <div>
-          <NavigationBar/>
-          <Greetings name={username}/>
-          <SignedIn>
-            <div className="mt-24">
-              <p className="mt-2 ml-20 font-semibold text-gray-800 dark:text-slate-200">Recommended Lessons for You</p>
-              <div className="ml-20 m-4 flex flex-row">
-                <Lesson href="https://neilp06.net" condition="homepage" lessonName="1.0 Intro to Java" description={desc} status="Not Started"/>
-                <Lesson condition="homepage" lessonName="1.1 Java Syntax" description={desc} status="In Progress..."/>
-                <Lesson condition="homepage" lessonName="1.2 Java Objects I" description={desc} status="Not Started"/>
-                <Lesson condition="homepage" lessonName="1.3 Java Objects II" description={desc} status="Not Started"/>
-              </div>
-            </div>
-            <div className="mt-20">
-              <p className="mt-2 ml-20 font-semibold text-gray-800 dark:text-slate-200">Practice Problems for You</p>
-              <div className="ml-20 m-4 flex flex-row">
-                <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="In Progress..."/>
-                <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="Not Started"/>
-                <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="Not Started"/>
-                <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="Not Started"/>
-              </div>
-            </div>
-          </SignedIn>
-          <SignedOut>
-            <h2>test</h2>
-          </SignedOut>
-          <Footer/>
-        </div>
+        <Content/>
       </ClerkProvider>
     );
   } else {
@@ -56,6 +27,47 @@ export default function App() {
       <div>
         <Warning/>
       </div>
+    );
+  }
+}
+
+function Content() {
+  const { user } = useUser();
+  const desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  if (user) {
+    return (
+      <SignedIn>
+        <NavigationBar/>
+          <Greetings name={user.firstName}/>
+          <div className="mt-24">
+            <p className="mt-2 ml-20 font-semibold text-gray-800 dark:text-slate-200">Recommended Lessons for You</p>
+            <div className="ml-20 m-4 flex flex-row">
+              <Lesson href="https://neilp06.net" condition="homepage" lessonName="1.0 Intro to Java" description={desc} status="Not Started"/>
+              <Lesson condition="homepage" lessonName="1.1 Java Syntax" description={desc} status="In Progress..."/>
+              <Lesson condition="homepage" lessonName="1.2 Java Objects I" description={desc} status="Not Started"/>
+              <Lesson condition="homepage" lessonName="1.3 Java Objects II" description={desc} status="Not Started"/>
+            </div>
+          </div>
+          <div className="mt-20">
+            <p className="mt-2 ml-20 font-semibold text-gray-800 dark:text-slate-200">Practice Problems for You</p>
+            <div className="ml-20 m-4 flex flex-row">
+              <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="In Progress..."/>
+              <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="Not Started"/>
+              <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="Not Started"/>
+              <PracticeProblem condition="homepage" problemName="Printing Madness!" prerequisite="1.0, 1.1" status="Not Started"/>
+            </div>
+          </div>
+        <Footer/>
+      </SignedIn>
+    );
+  } else {
+    return (
+      <SignedOut>
+        <NavigationBar/>
+        <p className="mt-24 ml-20 font-mono text-black dark:text-white font-bold text-5xl">Welcome to ProspectiveProgramming! 👏</p>
+          <h2>test</h2>
+        <Footer/>
+      </SignedOut>
     );
   }
 }
--- src/Customization.js
+++ src/Customization.js
@@ -1,17 +1,9 @@
 import React from "react";
 
 export default function Greetings(props) {
-    if (props.name !== undefined) {
         return (
-            <div className="mt-24">
-                <h2 className="ml-20 font-mono text-black dark:text-white font-bold text-6xl">Hello, {props.name}! 👋</h2>
-            </div>
-        );
-    } else {
-        return (
-            <div className="mt-24">
-                <h2 className="ml-20 font-mono text-black dark:text-white font-bold text-6xl">Hello, user!</h2>
-            </div>
-        )
-    }
+        <div className="mt-24">
+            <h2 className="ml-20 font-mono text-black dark:text-white font-bold text-6xl">Welcome back, {props.name}! 👋</h2>
+        </div>
+    );
 }
