57797b207541ae4485c59e7d632fa7fcf4162a57
NlytxGames <58278838+NlytxGames@users.noreply.github.com>
2023-08-19 20:24:10 -0500

    Added more content

--- src/index.js
+++ src/index.js
@@ -4,16 +4,25 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App';
 import LessonsApp from "./pages/Lessons.App.js";
+// imports Java practice-problem pages:
+import JavaProblemOne from './pages/java-problems/1.java.js';
+import JavaProblemTwo from './pages/java-problems/2.java.js';
+import JavaProblemThree from './pages/java-problems/3.java.js';
+import JavaProblemFour from './pages/java-problems/4.java.js';
+import JavaProblemFive from './pages/java-problems/5.java.js';
+import JavaProblemSix from './pages/java-problems/6.java.js';
+import JavaProblemSeven from './pages/java-problems/7.java.js';
+import JavaProblemEight from "./pages/java-problems/8.java.js";
+import JavaProblemNine from "./pages/java-problems/9.java.js";
+// imports Java lesson pages:
 import JavaOne from "./pages/java/1.java.js";
-import JavaProblemOne from './pages/java-problems/1.java';
-import JavaProblemTwo from './pages/java-problems/2.java';
-import JavaProblemThree from './pages/java-problems/3.java';
-import JavaProblemFour from './pages/java-problems/4.java';
-import JavaProblemFive from './pages/java-problems/5.java';
-
 import JavaTwo from "./pages/java/2.java.js";
 import JavaThree from "./pages/java/3.java.js";
 import JavaFour from "./pages/java/4.java.js";
+import JavaFive from "./pages/java/5.java.js";
+import JavaSix from "./pages/java/6.java.js";
+import JavaSeven from "./pages/java/7.java.js";
+// import ProspectiveProgramming learning hubs:
 import PracticeApp from "./pages/Practice.App.js";
 import RegistrationApp from "./pages/Registration.App.js";
 
@@ -51,6 +60,18 @@ const router = createBrowserRouter([
     element: <JavaFour/>
   }, 
   {
+    path: "java-lesson-five",
+    element: <JavaFive/>
+  },
+  {
+    path: "java-lesson-six",
+    element: <JavaSix/>
+  },
+  {
+    path: "java-lesson-seven",
+    element: <JavaSeven/>
+  },
+  {
     path: "java-practice-one",
     element: <JavaProblemOne/>
   }, 
@@ -69,6 +90,22 @@ const router = createBrowserRouter([
   {
     path: "java-practice-five",
     element: <JavaProblemFive/>
+  }, 
+  {
+    path: "java-practice-six",
+    element: <JavaProblemSix/>
+  }, 
+  {
+    path: "java-practice-seven",
+    element: <JavaProblemSeven/>
+  }, 
+  {
+    path: "java-practice-eight",
+    element: <JavaProblemEight/>
+  },
+  {
+    path: "java-practice-nine",
+    element: <JavaProblemNine/>    
   }
 ]); 
 
---
+++ src/pages/java-problems/7.java.js
@@ -0,0 +1,129 @@
+import { createClient } from "@supabase/supabase-js";
+import { ClerkProvider, useUser } from "@clerk/clerk-react";
+import PracticeModule from "../../layouts/PracticeProblem.layout.js";
+import React, { useEffect, useState } from "react";
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+    throw new Error("An error occured in relation to Clerk: no key found.");
+}
+const key = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
+
+export default function JavaProblemSeven() {
+    document.body.classList.add("bg-slate-300", "dark:bg-gray-900");
+    // following keys are initialized to fetch lesson data:
+    const [ expected, setExpected ] = useState("");
+    const [ practiceContent, setPracticeContent ] = useState("");
+    const [ practiceId, setPracticeId ] = useState("");
+    const [ practiceName, setPracticeName ] = useState("");
+    const [ prompt, setPrompt ] = useState("");
+    // loads lesson data from the database with a hook:
+    useEffect(() => {
+        const loadData = async(e) => {
+            // sets data array to a variable:
+            const { data, error } = await supabase.from("java-practice").select().eq("id", 7);
+            // throws an error if the data is corrupted:
+            if (error) {
+                throw new Error("An error occured in relation to Supabase fetch.");
+            }
+            // sets data to the following variables for later use:
+            setExpected(data[0].expectedOutput);
+            setPracticeContent(data[0].content);
+            setPracticeId(data[0].practiceId);
+            setPracticeName(data[0].name);
+            setPrompt(data[0].lessonProblem);
+        }
+        loadData();
+    }, []);
+    /**
+     * @param {*} data 
+     * --> when data is received, it calls a specific function:
+     */
+    const handleParentData = (data) => {
+        // checks to see if data arrives to console:
+        console.log("Data received to the parent module:", data);
+        ChangeToCompletedStatus();
+    };
+    // loading function when content is being fetched:
+    if (prompt === "") {
+        return (
+            <p className="mt-5 ml-5 font-semibold text-white">Content is loading... </p>
+        );
+    }
+    // returns parent module:
+    return (
+        <ClerkProvider publishableKey={key}>
+            <ChangeStatus/>
+            <PracticeModule expected={expected} practiceContent={practiceContent} practiceId={practiceId} practiceName={practiceName} prompt={prompt}  onSendData={handleParentData}/>
+        </ClerkProvider>
+    );
+}
+
+/**
+ * @returns <ChangeStatus/>
+ * --> creates a module that changes status when the user enters a lesson page.
+ */
+function ChangeStatus() {
+    // fetches user information:
+    const { user } = useUser();
+    // initializes a variable that will be used to fetch user status:
+    const [ userStatus, setUserStatus ] = useState("");
+    // updates lesson status with a hook:
+    useEffect(() => {
+        // checks if the user is logged on to prevent database errors:
+        if (user) {
+            // fetches current status data to ensure no interference with completed lessons:
+            const fetchData = async () => {
+                // fetches data:
+                const { data, error } = await supabase.from("users").select("java_p_seven").eq("userId", user.id);
+                // throws an error if a database error occurs:
+                if (error) {
+                    throw new Error("An error occured in relation to Supabase fetch.", error);
+                }
+                // sets data to the `userStatus` variable:
+                setUserStatus(data[0].java_p_seven);
+            }
+            // calls the function:
+            fetchData();
+            // gives the loadData callback a 2000ms (2s) timeout to ensure no latency bugs: 
+            setTimeout(() => {
+                // prevents initialized variables to change status data & prevents completed status collisions:
+                if (userStatus !== "Completed" && userStatus) {
+                    const loadData = async () => {
+                        // updates data and exports it as an error:
+                        const { error } = await supabase.from("users").update({ java_p_seven: "In Progress" }).eq("userId", user.id);
+                        // throws an error if a database error occurs:
+                        if (error) {
+                            throw new Error("An error occurred in relation to Supabase update.", error);
+                        }
+                    };
+                    loadData();
+                }
+            }, 2000);
+        }
+    }, [user, userStatus]);
+}
+
+/**
+ * @returns ChangeToCompletedStatus()
+ * --> creates a function that changes status when the user successfully completes the practice problem.
+ */
+function ChangeToCompletedStatus() {
+    // fetches user information:
+    const { user } = useUser();
+    // updates lesosn status with a hook:
+    useEffect(() => {
+        // checks if the user is logged on to prevent database errors:
+        if (user) {
+            const changeData = async () => {
+                // updates data and exports it as an error:
+                const { error } = await supabase.from("users").update({ java_p_seven: "Completed" }).eq("userId", user.id);
+                // throws an error if a database error occurs:
+                if (error) {
+                    throw new Error("An error occured in relation to Supabase update.", error);
+                }
+            };
+            changeData();
+        }  
+    }, [user]);  
+}
